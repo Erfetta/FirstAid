59378b5499d5aa881c0c5c9fb546ede21034f57c
Alessio Lavore <61512398+NotRambi@users.noreply.github.com>
2024-08-29 17:55:11 +0200

    Manuale operatore
    creata lista, e prime 3 pagine.
    Sia front end che back end

--- app/controllers/operatorpage/manual_controller.rb
+++ app/controllers/operatorpage/manual_controller.rb
@@ -1,7 +1,15 @@
 module Operatorpage
   class ManualController < ApplicationController
+    before_action :set_manualID , only: [:manual_element]
+    def manual_list
+
+    end
     def manual_element
         
+    end
+    private
+    def set_manualID
+      @manualID = params[:id]
     end
   end
 end
