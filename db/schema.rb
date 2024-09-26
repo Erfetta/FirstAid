f7432cebbbe7c351821031f5ebb6fd78a829d1d0
Erfetta <149378053+Erfetta@users.noreply.github.com>
2024-09-26 18:30:47 +0200

    Database
    risolto problema delle foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_25_164500) do
+ActiveRecord::Schema.define(version: 2024_09_26_162857) do
 
   create_table "operators", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -107,4 +107,9 @@ ActiveRecord::Schema.define(version: 2024_09_25_164500) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "operators", "supervisors"
+  add_foreign_key "report_dones", "operators"
+  add_foreign_key "report_dones", "users"
+  add_foreign_key "reports", "operators"
+  add_foreign_key "reports", "users"
 end
