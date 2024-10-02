1d3b0d56fa02ae5c472dc241f9907b425eb1a9f9
Erfetta <149378053+Erfetta@users.noreply.github.com>
2024-10-02 12:53:08 +0200

    database

--- db/migrate/20241001224954_create_data.rb
+++ db/migrate/20241001224954_create_data.rb
@@ -1,9 +1,9 @@
 class CreateData < ActiveRecord::Migration[6.1]
   def change
     create_table :data do |t|
-      t.string :tipo
-      t.float :valore
-      t.references :user, null: false, foreign_key: true  # Assicurati che il nome sia 'user'
+      t.string :measurement_type  # Cambiato da 'tipo' a 'measurement_type'
+      t.float :value              # Cambiato da 'valore' a 'value'
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_01_230459) do
+ActiveRecord::Schema.define(version: 2024_10_02_103604) do
 
   create_table "data", force: :cascade do |t|
-    t.string "tipo"
-    t.float "valore"
+    t.string "measurement_type"
+    t.float "value"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
